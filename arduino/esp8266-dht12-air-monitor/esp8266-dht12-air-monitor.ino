ae64368a895da0d97871460b42270c749a2e6956
Denis Rykovanov <denis.rykovanov@mail.ru>
2021-01-30 22:06:35 +0300

    simpliest integrated with dust sensor #6

--- arduino/esp8266-dht12-air-monitor/esp8266-dht12-air-monitor.ino
+++ arduino/esp8266-dht12-air-monitor/esp8266-dht12-air-monitor.ino
@@ -11,9 +11,10 @@
 #include "local_specific_variables.h"
 #include "time_utils.h"
 
-// note we should avoid reaching max mqttKeepAlive time, it is a reason why it is reduced twice
-#define PUBLISH_TIMEOUT 1000 * mqttKeepAlive/2
+// it will be replaced with a third-party lib later
+#include "dust_sensor.h"
 
+#define PUBLISH_TIMEOUT 1000/2 * mqttKeepAlive
 #define NTP_OFFSET   0      // In seconds
 #define NTP_INTERVAL 60 * 1000    // In miliseconds
 #define NTP_ADDRESS  "ru.pool.ntp.org"
@@ -26,13 +27,17 @@ NTPClient timeClient(ntpUDP, NTP_ADDRESS, NTP_OFFSET, NTP_INTERVAL);
 DHT12 dht12;
 
 char msg[400];
-const char* msgTemplate = "{\"TimeStamp\":\"%s\",\n \"Values\":\n[{\"Type\":\"Float\",\"Name\":\"Humidity\",\"Value\":null},\n {\"Type\":\"Float\",\"Name\":\"CarbonDioxide\",\"Value\":null},\n {\"Type\":\"Float\",\"Name\":\"Pressure\",\"Value\":null},\n {\"Type\":\"Float\",\"Name\":\"Temperature\",\"Value\":\"%d.%02d\"}]}";
+const char* msgTemplateDefault = "{\"TimeStamp\":\"%s\",\n \"Values\":\n[{\"Type\":\"Float\",\"Name\":\"Dust ug/m3\",\"Value\":\"%d.%02d\"},\n {\"Type\":\"Float\",\"Name\":\"Temperature\",\"Value\":\"%d.%02d\"}]}";
+const char* msgTemplateWithoutTemp = "{\"TimeStamp\":\"%s\",\n \"Values\":\n[\n {\"Type\":\"Float\",\"Name\":\"Dust ug/m3\",\"Value\":\"%d.%02d\"}\n]}";
+
 String topicRegistryCommands = String("$registries/")+String(yandexIoTCoreRegistryId)+String("/commands/#");
 String topicRegistryCommandLight = String("$registries/")+String(yandexIoTCoreRegistryId)+String("/commands/light");
 String topicEvents = String("$devices/")+String(yandexIoTCoreDeviceId)+String("/events");
 
-boolean needPublish = false;
+boolean needPublish = true;
 bool lightIsOn = false;
+
+ 
 
 WiFiClientSecure  net;
 PubSubClient client(net);
@@ -64,6 +69,8 @@ void connect() {
 }
 
 void setup() {
+
+  setupDustSensor();
 
   pinMode(LED_BUILTIN, OUTPUT);
   DEBUG_SERIAL.setTimeout(2000);
@@ -97,7 +104,8 @@ void messageReceived(char* topic, byte* payload, unsigned int length) {
     DEBUG_SERIAL.print("Handling light cmd");
     lightIsOn = payloadStr.equals("True");
     DEBUG_SERIAL.println(lightIsOn);
-    digitalWrite(LED_BUILTIN, lightIsOn ? LOW : HIGH);
+  
+    digitalWrite(LED_BUILTIN, lightIsOn ? LOW : HIGH);    
   } else {
     DEBUG_SERIAL.println("Unknown command");
   }
@@ -122,12 +130,20 @@ void loop() {
   }
 
   if(needPublish) {
+
+
+    
     if (lightIsOn) {
       digitalWrite(LED_BUILTIN, HIGH);
       delay(10);
       digitalWrite(LED_BUILTIN, LOW);
     }
+    
 
+    bool dht12Read = true;
+    float t12;
+    float h12;
+    
     DHT12::ReadStatus chk = dht12.readStatus();
     switch (chk) {
     case DHT12::OK:
@@ -165,50 +181,53 @@ void loop() {
     }
     
     // Read temperature as Celsius (the default)
-    float t12 = dht12.readTemperature();
-    // Read temperature as Fahrenheit (isFahrenheit = true)
-    float f12 = dht12.readTemperature(true);
+    t12 = dht12.readTemperature();
     // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
-    float h12 = dht12.readHumidity();
+    h12 = dht12.readHumidity();
 
-    bool dht12Read = true;
+    
     // Check if any reads failed and exit early (to try again).
 
-    if (isnan(h12) || isnan(t12) || isnan(f12)) {
+    if (isnan(h12) || isnan(t12)) {
       Serial.println("Failed to read from DHT12 sensor!");
-
       dht12Read = false;
     }
 
-    if (dht12Read){
-      // Compute heat index in Fahrenheit (the default)
-      float hif12 = dht12.computeHeatIndex(f12, h12);
-      // Compute heat index in Celsius (isFahreheit = false)
-      float hic12 = dht12.computeHeatIndex(t12, h12, false);
-      // Compute dew point in Fahrenheit (the default)
-      float dpf12 = dht12.dewPoint(f12, h12);
-      // Compute dew point in Celsius (isFahreheit = false)
-      float dpc12 = dht12.dewPoint(t12, h12, false);
 
-      timeClient.update();
-      String formattedTime = getFormattedDate(timeClient.getEpochTime());
+    timeClient.update();
 
-      snprintf(msg, sizeof(msg), msgTemplate,
+    String formattedTime = getFormattedDate(timeClient.getEpochTime());
+    
+    float dustValue = measureDustSensorValue();
+    
+    if (!dht12Read) {
+      snprintf(msg, sizeof(msg), msgTemplateWithoutTemp,
         formattedTime.c_str(),
-        (int)t12, (int)(t12*100)%100   
+        (int)dustValue, (int)(dustValue*100)%100
       );
-
-      DEBUG_SERIAL.println(msg);
-      
-      if (client.publish(topicEvents.c_str(), msg)) {        
-          DEBUG_SERIAL.println(topicEvents);
-          DEBUG_SERIAL.println("Publish ok");
-      } else {
-        DEBUG_SERIAL.println("Publish failed");
-      }
-
+    } else {
+      snprintf(msg, sizeof(msg), msgTemplateDefault,
+        formattedTime.c_str(),
+        (int)dustValue, (int)(dustValue*100)%100,
+        (int)t12, (int)(t12*100)%100   
+      );      
     }
+    
+
+  
   }
+
+
+
+  DEBUG_SERIAL.println(msg);
+      
+//      if (client.publish(topicEvents.c_str(), msg)) {        
+//          DEBUG_SERIAL.println(topicEvents);
+//          DEBUG_SERIAL.println("Publish ok");
+//      } else {
+//        DEBUG_SERIAL.println("Publish failed");
+//      }
+  
 
   DEBUG_SERIAL.print("sleep for ");
   DEBUG_SERIAL.println(PUBLISH_TIMEOUT);
